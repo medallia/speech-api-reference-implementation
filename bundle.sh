f8a98e4347c7ad751388abf8783a6f49ad61ccbe
Chris Verges <cverges@medallia.com>
2022-08-28 11:26:15 -0400

    Bumped bundle version

--- bundle.sh
+++ bundle.sh
@@ -5,7 +5,7 @@ jpackage \
   --main-jar speech-api-upload-1.1-SNAPSHOT.jar \
   --vendor "Medallia, Inc." \
   --name medallia-speech \
-  --app-version 1.1.0 \
+  --app-version 1.2.0 \
   --copyright "(c) 2021,2022 Medallia, Inc." \
   --description "A reference implementation for Medallia Speech" \
   --license-file LICENSE.md \
